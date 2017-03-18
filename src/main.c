544ea61a1429a621b796fe1e780e846bb2b7f7ad
Angel <asolis@e1z2r4p7.42.us.org>
2017-03-17 22:15:56 -0700

    4th fdf push

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ static void	reset_map(t_fdf *f)
 	f->coords.angle_y = cos(M_PI / 3);
 	f->coords.angle_z = f->coords.angle_y * sin(M_PI / 6);
 	f->coords.zoom = (f->coords.xs < 99) ? 100 - f->coords.ys : 8;
-	f->coords.iso = 3;
+	f->coords.iso = 20;
 	f->colors.red = 0x4F;
 	f->colors.green = 0x4F;
 	f->colors.blue = 0x4F;
@@ -34,12 +34,21 @@ static void	random_color(t_fdf *f)
 	f->colors.blue = (rand() % 0x7F);
 }
 
-static int my_key_funct(int keycode, t_fdf *f)
+int	my_key_(int keycode, t_fdf *f)
 {
 	if (keycode == 53)
 		exit(0);
-	else if (keycode == 46)
-			random_color(f);
+	else if (keycode == 12)
+		f->coords.x_val += 1;
+	else if (keycode == 14)
+		f->coords.x_val -= 1;
+	return (0);
+}
+
+static int my_key_funct(int keycode, t_fdf *f)
+{
+	if (keycode == 8)
+		random_color(f);
 	else if (keycode == 15)
 			reset_map(f);
 	else if (keycode == 126)
@@ -50,27 +59,52 @@ static int my_key_funct(int keycode, t_fdf *f)
 			f->coords.coord_y += 7;
 	else if (keycode == 124)
 			f->coords.coord_y -= 7;
-	printf("%d\n",keycode );
+	else if (keycode == 13)
+			f->coords.angle_z += 0.1;
+	else if (keycode == 1)
+			f->coords.angle_z -= 0.1;
+	else if (keycode == 0)
+			f->coords.angle_y += 0.02;
+	else if (keycode == 2)
+			f->coords.angle_y -= 0.02;
+	else if (keycode == 6)
+			f->coords.zoom += 10;
+	else if (keycode == 7)
+			f->coords.zoom -= 10;
+			printf("%d\n",keycode );
 	return (0);
 }
 // w = 13, a = 0, s = 1, d = 2, 126, 123, 125, 124
-void	drawing_base(int x, int y, void *mlx, void *win)
+void	drawing_instructions()
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while(j <= y)
-	{
-		while(i <= x)
-		{
-			mlx_pixel_put(mlx, win, i, j, 0x66FFFF);
-			i++;
-		}
-		i = 0;
-		j++;
-	}
+	ft_putstr("|----|FDF --------- MENU|-----|\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Use the arrows of your       |\n");
+	ft_putstr("|keyborad to move the map.    |\n");
+	ft_putstr("| [^] [<] [>] [down]          |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'R' to remake the map. |\n");
+	ft_putstr("|NOTE: The map it's gona have |\n");
+	ft_putstr("|same color and the same posi |\n");
+	ft_putstr("|tion that had in the beginnig|\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'C' to randoom colors. |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'W' to rotate the map. |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'S' to rotate the map. |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'A' to expand the map. |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'D' to contract the map|\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'Z' to zoom in the map |\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'X' to zoom out the map|\n");
+	ft_putstr("|-----------------------------|\n");
+	ft_putstr("|Press 'ESC to exit and close |\n");
+	ft_putstr("|the window.                  |\n");
+	ft_putstr("|-----------------------------|\n");
 }
 
 int		main(int argc, char *argv[])
@@ -80,11 +114,13 @@ int		main(int argc, char *argv[])
 	f = (t_fdf*)malloc(sizeof(t_fdf));
 	if (argc == 2)
 	{
+		drawing_instructions();
 		f->coords.fd = open(argv[1], O_RDONLY);
 		coordinates(f->coords.fd, f, argv[1]);
 		f->mlx.mlx = mlx_init();
 		f->mlx.win = mlx_new_window(f->mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "FDF");
 		reset_map(f);
+		mlx_key_hook(f->mlx.win, my_key_, f);
 		mlx_hook(f->mlx.win, 2, 3, my_key_funct, f);
 		mlx_loop_hook(f->mlx.mlx, fdf_map, f);
 		mlx_loop(f->mlx.mlx);
